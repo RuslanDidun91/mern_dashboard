1248c0190e495f0335f4025cd611b67368b62281
RuslanDidun <ruslandidun5@gmail.com>
2023-03-02 19:34:47 -0700

    google OAuth

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from "react";
 import { useLogin } from "@pankod/refine-core";
 import { Container, Box } from "@pankod/refine-mui";
-
 import { CredentialResponse } from "../interfaces/google";
+import { yariga } from "assets";
 
 export const Login: React.FC = () => {
   const { mutate: login } = useLogin<CredentialResponse>();
@@ -18,7 +18,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: "your-client-id",
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
@@ -41,10 +41,7 @@ export const Login: React.FC = () => {
   return (
     <Box
       component="div"
-      sx={{
-        background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
-        backgroundSize: "cover",
-      }}
+      sx={{ backgroundColor: '#FCFCFC' }}
     >
       <Container
         component="main"
@@ -65,7 +62,7 @@ export const Login: React.FC = () => {
           }}
         >
           <div>
-            <img src="./refine.svg" alt="Refine Logo" />
+            <img src={yariga} alt="Yariga Logo" />
           </div>
           <Box mt={4}>
             <GoogleButton />
