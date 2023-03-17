6862bddb2bdd965490989104e075414bf1369543
RuslanDidun <ruslandidun5@gmail.com>
2023-03-16 18:33:12 -0600

    PropertyCard component

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -1,5 +1,16 @@
 import Property from "../mongodb/models/property.js";
 import User from "../mongodb/models/user.js";
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export {
   getAllProperties,
@@ -11,7 +22,12 @@ export {
 
 
 const getAllProperties = async (req, res) => {
-
+  try {
+    const properties = await Property.find({}).limit(req.query._end);
+    res.status(200).json(properties);
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 };
 
 const getPropertyDetails = async (req, res) => {
@@ -19,7 +35,36 @@ const getPropertyDetails = async (req, res) => {
 };
 
 const createProperty = async (req, res) => {
+  try {
+    const { title, description, propertyType, location, price, photo, email } = req.body;
+    //mongoose library to grouped multiple info 
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
+    const user = await User.findOne({ email }).session(session);
+
+    if (!user) throw new Error("User not found");
+
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newProperty = await Property.create({
+      title,
+      description,
+      propertyType,
+      location,
+      price,
+      photo: photoUrl.url,
+      creator: user._id,
+    });
+
+    user.allProperties.push(newProperty._id);
+    await user.save({ session });
+    await session.commitTransaction();
+
+    res.status(200).json({ message: "Property created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const updateProperty = async (req, res) => {
